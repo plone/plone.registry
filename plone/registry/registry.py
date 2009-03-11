56adbe8c1f609a18cd650e13e29d0cc56e8ea321
Martin Aspeli <optilude@gmail.com>
2009-03-11 03:26:48 +0000

    Somehow, this was never committed
    svn path=/plone.registry/trunk/; revision=25797

--- plone/registry/registry.py
+++ plone/registry/registry.py
@@ -14,28 +14,6 @@ from plone.registry.record import Record
 from plone.registry.recordsproxy import RecordsProxy
 from plone.registry.events import RecordAddedEvent, RecordRemovedEvent
 
-class Records(OOBTree):
-    """The records stored in the registry
-    """
-    
-    def __setitem__(self, name, record):
-        if not _isdotted(name):
-            raise InvalidDottedName(record)
-        if not IRecord.providedBy(record):
-            raise ValueError("Value must be a record")
-        if not IPersistentField.providedBy(record.field):
-            raise ValueError("The record's field must be an IPersistentField.")
-        
-        record.__name__ = name
-        record.__parent__ = self.__parent__
-        super(Records, self).__setitem__(name, record)
-        notify(RecordAddedEvent(record))
-    
-    def __delitem__(self, name):
-        record = self[name]
-        super(Records, self).__delitem__(name)
-        notify(RecordRemovedEvent(record))
-    
 class Registry(Persistent):
     """The persistent registry
     """
@@ -43,8 +21,7 @@ class Registry(Persistent):
     implements(IRegistry)
     
     def __init__(self):
-        self._records = Records()
-        self._records.__parent__ = self
+        self._records = Records(self)
     
     # Basic value access API
     
@@ -92,3 +69,100 @@ class Registry(Persistent):
                 raise TypeError("There is no persistent field equivalent for "
                                 "the field `%s` of type `%s`." % (name, field.__class__.__name__))
             self.records[record_name] = Record(persistent_field, interface=interface, field_name=name)
+
+class Records(Persistent):
+    """The records stored in the registry
+    """
+    
+    __parent__ = None
+    
+    def __init__(self, parent):
+        self.__parent__ = parent
+        self.data = OOBTree()
+
+    def __setitem__(self, name, record):
+        if not _isdotted(name):
+            raise InvalidDottedName(record)
+        if not IRecord.providedBy(record):
+            raise ValueError("Value must be a record")
+        if not IPersistentField.providedBy(record.field):
+            raise ValueError("The record's field must be an IPersistentField.")
+        
+        record.__name__ = name
+        record.__parent__ = self.__parent__
+        self.data[name] = record
+        notify(RecordAddedEvent(record))
+    
+    def __delitem__(self, name):
+        record = self[name]
+        del self.data[name]
+        notify(RecordRemovedEvent(record))
+
+    # Unfortunately, you can't just subclass an OOBTree if you want to
+    # persist it...
+
+    def __getitem__(self, name):
+        return self.data.__getitem__(name)
+    
+    def get(self, name, default=None):
+        return self.data.get(name, default)
+    
+    def __nonzero__(self):
+        return self.data.__nonzero__()
+    
+    def __len__(self):
+        return self.data.__len__()
+        
+    def __iter__(self):
+        return self.data.__iter__()
+    
+    def __getslice__(self, index1, index2):
+        return self.data.__getslice__(index1, index2)
+        
+    def has_key(self, name):
+        return self.data.has_key(name)
+        
+    def __contains__(self, name):
+        return self.data.__contains__(name)
+        
+    def keys(self, min=None, max=None, excludemin=False, excludemax=False):
+        return self.data.keys(min, max, excludemin, excludemax)
+        
+    def maxKey(self, key=None):
+        return self.data.maxKey(key)
+        
+    def minKey(self, key=None):
+        return self.data.minKey(key)
+        
+    def values(self, min=None, max=None, excludemin=False, excludemax=False):
+        return self.data.values(min, max, excludemin, excludemax)
+        
+    def items(self, min=None, max=None, excludemin=False, excludemax=False):
+        return self.data.items(min, max, excludemin, excludemax)
+    
+    def update(self, collection):
+        self.data.update(collection)
+        
+    def byValue(self, minValue):
+        self.data.byValue(minValue)
+        
+    def setdefault(self, key, d):
+        return self.data.setdefault(key, d)
+        
+    def pop(self, key, d):
+        return self.data.pop(key, d)
+        
+    def insert(self, key, value):
+        self.data.insert(key, value)
+        
+    def difference(self, c1, c2):
+        return self.data.difference(c1, c2)
+        
+    def union(self, c1, c2):
+        return self.data.union(c1, c2)
+    
+    def intersection(self, c1, c2):
+        return self.data.intersection(c1, c2)
+    
+    def clear(self):
+        self.data.clear()
