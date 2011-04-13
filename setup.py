9ae9c8f17d02978f787ab9197752fdd5c47a0123
WouterVH <WouterVH@localhost>
2011-04-13 23:54:49 +0000

    move docs/HISTORY.txt to CHANGES.txt
    svn path=/plone.registry/trunk/; revision=48805

--- setup.py
+++ setup.py
@@ -1,16 +1,30 @@
-from setuptools import setup, find_packages
 import os
+from setuptools import setup, find_packages
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 version = '1.0b6'
+
+
+long_description = (
+    read('README.txt')
+    + '\n' +
+    read('plone', 'registry', 'registry.txt')
+    + '\n' +
+    read('plone', 'registry', 'events.txt')
+    + '\n' +
+    read('plone', 'registry', 'field.txt')
+    + '\n' +
+    read('CHANGES.txt')
+    + '\n'
+    )
 
 setup(name='plone.registry',
       version=version,
       description="A debconf-like (or about:config-like) registry for storing application settings",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read() +
-                       open(os.path.join("plone", "registry", "registry.txt")).read() +
-                       open(os.path.join("plone", "registry", "events.txt")).read() + 
-                       open(os.path.join("plone", "registry", "field.txt")).read(),
+      long_description=long_description,
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
