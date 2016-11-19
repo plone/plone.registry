cac414aa84d5dbbf88a4cc4c6bebc4d5245238eb
Maurits van Rees <maurits@vanrees.org>
2016-11-19 21:24:25 +0100

    Back to development: 1.1.2
    [ci skip]

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import os
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.1.1'
+version = '1.1.2.dev0'
 
 description = "Registry for application settings (like debconf/ about:config)"
 long_description = (
