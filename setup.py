8059a636179e6bae23a179e6916fd2a4574e0231
Gil Forcada Codinachs <gil.gnome@gmail.com>
2016-12-02 09:33:31 +0100

    Update trove classifiers

--- setup.py
+++ setup.py
@@ -32,8 +32,10 @@ setup(
     classifiers=[
         "Framework :: Plone",
         "Framework :: Plone :: 5.1",
+        "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     keywords='configuration registry',
