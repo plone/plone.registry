234d0e1c86cf56b403af50c8f2037f4ab83bd825
Maurits van Rees <maurits@vanrees.org>
2025-09-10 17:25:34 +0200

    Back to development: 2.0.3
    [ci skip]

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = "2.0.2"
+version = "2.0.3.dev0"
 
 description = "Registry for application settings (like debconf/ about:config)"
 long_description = (
